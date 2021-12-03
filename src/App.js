6d419e1bec83b4c97b7c88e21d958c25ff4655e1
Akash Soni <akashkumarsonir6@gmail.com>
2021-12-04 01:03:31 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import "./App.css";
 import { useState } from "react";
 function App() {
-  const [Counter, setCounter] = useState(10);
+  const [Counter, setCounter] = useState(0);
   return (
     <div className="App">
       <div className="container">
-        <h1>Counter App</h1>
-        <h1>
-          <span>{Counter}</span>
-        </h1>
-        <button
-          className="btn btn-primary"
-          onClick={() => setCounter(Counter + 1)}
-        >
-          Increment
-        </button>
-        <br />
-        <br />
-        <button
-          className="btn btn-info "
-          onClick={() => setCounter(Counter - 1)}
-        >
-          Decrement
-        </button>
+        <div className="display">
+          <h1>Counter App</h1>
+          <h1>
+            <span>{Counter}</span>
+          </h1>
+        </div>
+        <div className="action">
+          <button
+            className="btn btn-primary"
+            onClick={() => setCounter(Counter + 1)}
+          >
+            Increment +1
+          </button>
+
+          <button
+            className="btn btn-info "
+            onClick={() => setCounter(Counter - 1)}
+          >
+            Decrement -1
+          </button>
+          <button className="btn btn-danger" onClick={() => setCounter(0)}>
+            Reset
+          </button>
+        </div>
       </div>
     </div>
   );
